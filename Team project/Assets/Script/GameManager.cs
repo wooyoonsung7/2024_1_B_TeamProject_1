337c9d1b23f145a7703d6bc86f485cfbf6571f9f
wooyoonsung7 <suger8831@gmail.com>
2024-05-21 18:06:32 +0900

    Fix : 게임 타일 리스트 제작

---
+++ Team project/Assets/Script/GameManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject[] MapBlock = new GameObject[10]; 
+        
+
+}
