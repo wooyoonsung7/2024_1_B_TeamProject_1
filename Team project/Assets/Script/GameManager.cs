f0977f057618d71b50b405589fe20d8e8c26296f
wooyoonsung7 <suger8831@gmail.com>
2024-05-21 18:13:42 +0900

    Fix : 주석

--- Team project/Assets/Script/GameManager.cs
+++ Team project/Assets/Script/GameManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
+
 
 public class GameManager : MonoBehaviour
 {
-
+    // 윤성아 이라고 되있는거 너가 원하는 데로 지정해
     public GameObject[] MapBlock = new GameObject[10]; 
-        
+    
 
 }
