d878801eefcbbbcbe746d81f9260f75ccff6c650
jinjooyoung <jjy26297449@gmail.com>
2024-06-20 12:44:17 +0900

    [Fix] 카드 UI 수정, 몬스터 수 UI 추가, 튜토리얼 수정

--- Team project/Assets/Script/UI/WaveUI.cs
+++ Team project/Assets/Script/UI/WaveUI.cs
@@ -10,9 +10,12 @@ public class WaveUI : MonoBehaviour
 
     public Text wave;
     public Text nextWaveTimer;
+    public Text enemyNum;
     private int stageWave;
     private int currentWave;
     private float stateTimer;
+    private int enemyMax;
+    private int enemyCursor;
 
     // Start is called before the first frame update
     void Start()
@@ -26,8 +29,12 @@ public class WaveUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // 현재 웨이브 숫자 알려주는 UI
         if (GameManager.Instance.isAllWaveDone)
+        {
+            enemyNum.text = "웨이브 종료";
             return;
+        }
 
         if (currentWave != stageWave)
         {
@@ -38,6 +45,8 @@ public class WaveUI : MonoBehaviour
         if (currentWave == stageWave)
             wave.text = stageWave + " / " + stageWave;
 
+
+        // 다음 웨이브까지 시간 알려주는 UI
         stateTimer = GameManager.Instance.stateTimer;
         if (stateTimer > 1.0f)
         {
@@ -51,5 +60,51 @@ public class WaveUI : MonoBehaviour
         {
             nextWaveTimer.text = currentWave + " 웨이브 진행 중";
         }
+
+
+        // 웨이브 마다 몬스터 수 알려주는 UI
+        if (currentWave == 1)
+        {
+            enemyMax = GameManager.Instance.stageWaveMenberMax[0];
+            enemyCursor = GameManager.Instance.stageWaveMenberCursor[0];
+
+            enemyNum.text = enemyCursor + " / " + enemyMax;
+        }
+        else if (currentWave == 2)
+        {
+            enemyMax = GameManager.Instance.stageWaveMenberMax[1];
+            enemyCursor = GameManager.Instance.stageWaveMenberCursor[1];
+
+            enemyNum.text = enemyCursor + " / " + enemyMax;
+        }
+        else if (currentWave == 3)
+        {
+            enemyMax = GameManager.Instance.stageWaveMenberMax[2];
+            enemyCursor = GameManager.Instance.stageWaveMenberCursor[2];
+
+            enemyNum.text = enemyCursor + " / " + enemyMax;
+        }
+        else if (currentWave == 4)
+        {
+            enemyMax = GameManager.Instance.stageWaveMenberMax[3];
+            enemyCursor = GameManager.Instance.stageWaveMenberCursor[3];
+
+            enemyNum.text = enemyCursor + " / " + enemyMax;
+        }
+        else if (currentWave == 5)
+        {
+            enemyMax = GameManager.Instance.stageWaveMenberMax[4];
+            enemyCursor = GameManager.Instance.stageWaveMenberCursor[4];
+
+            enemyNum.text = enemyCursor + " / " + enemyMax;
+        }
+        else if (currentWave == 0)
+        {
+            if (GameManager.Instance.isAllWaveDone == true)
+            {
+                enemyNum.text = "웨이브 종료";
+            }
+        }
+        
     }
 }
